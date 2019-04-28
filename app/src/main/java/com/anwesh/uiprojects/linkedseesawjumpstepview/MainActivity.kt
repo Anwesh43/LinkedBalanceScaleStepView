e32eeee65f764c57497f222fe3143ec6ba37d343
Anwesh43 <anweshthecool0@gmail.com>
2019-04-29 02:04:54 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedseesawjumpstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedseesawjumpstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedseesawjumpstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.balancescalestepview.BalanceScaleStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BalanceScaleStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
