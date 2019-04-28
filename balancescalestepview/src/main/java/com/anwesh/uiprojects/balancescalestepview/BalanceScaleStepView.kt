0d9762b33df8e05d7daea11a695cb943bd38482c
Anwesh43 <anweshthecool0@gmail.com>
2019-04-29 01:59:15 +0530

    adding BalanceScaleStep class

--- balancescalestepview/src/main/java/com/anwesh/uiprojects/balancescalestepview/BalanceScaleStepView.kt
+++ balancescalestepview/src/main/java/com/anwesh/uiprojects/balancescalestepview/BalanceScaleStepView.kt
@@ -174,4 +174,28 @@ class BalanceScaleStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BalanceScaleStep(var i : Int) {
+
+        private val root : BSSNode = BSSNode(0)
+        private var curr : BSSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
