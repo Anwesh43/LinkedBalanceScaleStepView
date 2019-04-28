f9747834b4f1cfbc63312e8d52d7bc57d8abe7eb
Anwesh43 <anweshthecool0@gmail.com>
2019-04-29 01:50:29 +0530

    adding drawBSSNode canvas extension method

--- balancescalestepview/src/main/java/com/anwesh/uiprojects/balancescalestepview/BalanceScaleStepView.kt
+++ balancescalestepview/src/main/java/com/anwesh/uiprojects/balancescalestepview/BalanceScaleStepView.kt
@@ -20,6 +20,8 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val bssFactor : Float = 6f
+val bssWidthFactor : Float = 7f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -30,3 +32,35 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawScalePivots(x : Float, y : Float, size : Float, paint : Paint) {
+    save()
+    translate(x, 0f)
+    drawLine(0f, 0f, size, y, paint)
+    drawLine(size, y, 2 * size, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val bssSize : Float = size / bssFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        scale(1f - 2 * j, 1f)
+        drawScalePivots(-size - size / bssWidthFactor, -bssSize * sc, size / bssWidthFactor, paint)
+        restore()
+    }
+    drawLine(-size, -bssSize * sc1.divideScale(0, 2), size, -bssSize * sc1.divideScale(1, 2), paint)
+    restore()
+}
