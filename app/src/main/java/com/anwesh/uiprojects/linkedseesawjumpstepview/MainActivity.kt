6915ce10fb180ebe8c0ca542775cad1665ee102f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-29 02:04:14 +0530

    adidng view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedseesawjumpstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedseesawjumpstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedseesawjumpstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.balancescalestepview.BalanceScaleStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BalanceScaleStepView.create(this)
     }
 }
